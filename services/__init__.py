5c5c0b457dd1d03bc8cc672b09285378846ca817
24luobo <3615483771@qq.com>
2026-06-30 16:47:22 +0800

    Add services/__init__.py

---
+++ services/__init__.py
@@ -0,0 +1,11 @@
+from .user_service import UserService
+from .clothing_service import ClothingService
+from .recommendation_service import RecommendationService
+from .favorite_service import FavoriteService
+
+__all__ = [
+    'UserService',
+    'ClothingService',
+    'RecommendationService',
+    'FavoriteService'
+]
